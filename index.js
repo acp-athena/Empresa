f9674ad18c2ecac880969a493bc64105ed56f781
Ana Carolina Almeida Paula <anacpaula83@gmail.com>
2024-05-24 20:43:10 -0300

    Ultimas finalizações

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ function cadastro(req, res){
         <body>
             <div class="container m-5">
                 <form method="POST" action='/cadastro' class="border row g-3 needs-validation" novalidate>
-                    <legend>Cadastro</legend>
+                    <h1>Cadastro</h1>
                     <div class="col-md-4">
-                        <label for="cnpj" class="form-label">cnpj:</label>
-                        <input type="text" class="form-control" id="cnpj" name="cnpj" value="${cnpj}" required>`);
+                        <label for="cnpj" class="form-label">CNPJ:</label>
+                        <input type="number" placeholder="Ex: XX. XXX. XXX/0001-XX" class="form-control" id="cnpj" name="cnpj" value="${cnpj}" required>`);
         if (cnpj == ""){
             res.write(`
                         <div m-2 class="alert alert-danger" role="alert">
@@ -62,20 +62,22 @@ function cadastro(req, res){
             `);
         }
         res.write(`</div>
+                        <br>
                         <div class="col-md-4">
-                            <label for="razaoSocial" class="form-label">razaoSocial</label>
-                            <input p-1 type="text" class="form-control" id="razaoSocial" name="razaoSocial" value="${razaoSocial}" required>`);
+                            <label for="razaoSocial" class="form-label">Razão Social</label>
+                            <input p-1 type="text" class="form-control" id="razaoSocial" placeholder="Ex: InovaTech Soluções Inteligentes Ltda." name="razaoSocial" value="${razaoSocial}" required>`);
         if (razaoSocial == ""){
             res.write(`<div m-2 class="alert alert-danger" role="alert">
                                 Por favor, informe a razão social da empresa.
                             </div>`);
         }        
         res.write(`
+                        <br>
                         </div>
                         <div class="col-md-4">
                             <label for="nomeFantasia" class="form-label">Nome fantasia da empresa</label>
                             <div class="input-group has-validation">
-                                <input type="text" class="form-control" id="nomeFantasia" name="nomeFantasia" value="${nomeFantasia}" aria-describedby="inputGroupPrepend" required>
+                                <input type="text" class="form-control" placeholder="Ex: NuvemGenius Tecnologia & Inovação" id="nomeFantasia" name="nomeFantasia" value="${nomeFantasia}" aria-describedby="inputGroupPrepend" required>
         `);            
         if (nomeFantasia == ""){
             res.write(`<div class="alert alert-danger" role="alert">
@@ -84,9 +86,10 @@ function cadastro(req, res){
         }
         res.write(`    </div>
                         </div>
+                        <hr>
                         <div class="col-md-6">
-                            <label for="endereço" class="form-label">endereço</label>
-                            <input type="text" class="form-control" id="endereço" name="endereço" value="${endereço}" required>`
+                            <label for="endereço" class="form-label">Endereço</label>
+                            <input type="text" class="form-control" id="endereço" placeholder="Ex: Rua Exemplo nº00" name="endereço" value="${endereço}" required>`
         );
         if (endereço == ""){
             res.write(`<div class="alert alert-danger" role="alert">
@@ -94,10 +97,11 @@ function cadastro(req, res){
                             </div>`);
         }
         res.write(`    </div>
-                        </div>
+
+                        <br>
                         <div class="col-md-6">
                             <label for="cidade" class="form-label">Cidade</label>
-                            <input type="text" class="form-control" id="cidade" name="cidade" value="${cidade}" required>`
+                            <input type="text" class="form-control" placeholder="Ex: São Paulo" id="cidade" name="cidade" value="${cidade}" required>`
         );
         if (cidade == ""){
             res.write(`<div class="alert alert-danger" role="alert">
@@ -105,10 +109,11 @@ function cadastro(req, res){
                             </div>`);
         }
         res.write(`</div>
+                        <br>
                         <div class="col-md-3">
                             <label for="uf" class="form-label">UF</label>
                             <select class="form-select" id="uf" name="uf" required>
-                                <option selected disabled value=${uf}>Escolha um uf...</option>
+                                <option selected disabled value=${uf}>Estado</option>
                                 <option value="AC">AC</option>
                                 <option value="AL">AL</option>
                                 <option value="AP">AP</option>
@@ -144,36 +149,41 @@ function cadastro(req, res){
                             </div>`);
         }
         res.write(`</div>
+                        <br>
                         <div class="col-md-3">
                             <label for="cep" class="form-label">CEP da empresa</label>
-                            <input type="text" class="form-control" id="cep" name="cep" value="${cep}" required>`);
+                            <input type="text" class="form-control" placeholder="Ex: XXXXX-XXX" id="cep" name="cep" value="${cep}" required>`);
         if (cep == ""){
             res.write(`<div class="alert alert-danger" role="alert">
                                 Por favor, informe o cep.
                             </div>`);
         }
         res.write(`</div>
+                        <br>
                         <div class="col-md-3">
                             <label for="email" class="form-label">Email da empresa</label>
-                            <input type="email" class="form-control" id="email" name="email" value="${email}" required>`);
+                            <input type="email" class="form-control" placeholder="Ex: exemplo@exemplo.com" id="email" name="email" value="${email}" required>`);
         if (email == ""){
             res.write(`<div class="alert alert-danger" role="alert">
                                 Por favor, informe o email da empresa.
                             </div>`);
         }
         res.write(`</div>
+                        <br>
                         <div class="col-md-3">
                             <label for="telefone" class="form-label">Telefone da empresa</label>
-                            <input type="number" class="form-control" id="telefone" name="telefone" value="${telefone}" required>`);
+                            <input type="number" placeholder="Ex: 99999-9999" class="form-control" id="telefone" name="telefone" value="${telefone}" required>`);
         if (telefone == ""){
-            res.write(`<div class="alert alert-danger" role="alert">
+            res.write(`<div  class="alert alert-danger" role="alert">
                                 Por favor, informe o telefone da empresa.
                             </div>`);
         }
         res.write(`</div>
+                    </div>
+                        <br>
                         <div class="col-12 mb-3">
                             <button class="btn btn-primary" type="submit">Cadastrar</button>
-                            <a class="btn btn-secondary" href="/">Voltar</a>                   
+                            <a href="/"><button class="btn btn-secondary">Voltar</a></button>                 
                         </div>
                     </form>
                 </div>
@@ -190,7 +200,7 @@ app.get('/lista', (req,res)=>{
     res.write('<head>');
     res.write('<title>Resultado do cadastro</title>');
     res.write('<meta charset="utf-8">');
-    res.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cyborg/bootstrap.min.css" integrity="sha384-nEnU7Ae+3lD52AK+RGNzgieBWMnEfgTbRHIwEvp1XXPdqdO6uLTd/NwXbzboqjc2" crossorigin="anonymous">')
+    res.write('<link rel="stylesheet" href="styleLista.css">')
     res.write('</head>');
     res.write('<body>');
     res.write('<h1>Lista de empresas</h1>');
@@ -208,21 +218,20 @@ app.get('/lista', (req,res)=>{
     res.write('</tr>');
     for (let i=0; i<lista.length; i++){
         res.write('<tr>');
-        res.write(`<td>${lista[i].cnpj}`);
-        res.write(`<td>${lista[i].razaoSocial}`);
-        res.write(`<td>${lista[i].nomeFantasia}`);
-        res.write(`<td>${lista[i].endereço}`);
-        res.write(`<td>${lista[i].cidade}`);
-        res.write(`<td>${lista[i].uf}`);
-        res.write(`<td>${lista[i].cep}`);
-        res.write(`<td>${lista[i].email}`);
-        res.write(`<td>${lista[i].telefone}`);
+        res.write(`<td>${lista[i].cnpj}</td>`);
+        res.write(`<td>${lista[i].razaoSocial}</td>`);
+        res.write(`<td>${lista[i].nomeFantasia}</td>`);
+        res.write(`<td>${lista[i].endereço}</td>`);
+        res.write(`<td>${lista[i].cidade}</td>`);
+        res.write(`<td>${lista[i].uf}</td>`);
+        res.write(`<td>${lista[i].cep}</td>`);
+        res.write(`<td>${lista[i].email}</td>`);
+        res.write(`<td>${lista[i].telefone}</td>`);
         res.write('</tr>');
     }
     res.write('</table>');
     res.write('<a href="/">Voltar</a>');
     res.write('</body>');
-    res.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>')
     res.write('</html>');
     res.end();
 });
